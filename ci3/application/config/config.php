396e5cc37dd3fca275d2cb823dff70432a7de292
kukuhyulionsetiaprakoso <kukuhyulionsetiaprakoso@gmail.com>
2023-04-18 20:40:52 +0700

    update

--- ci3/application/config/config.php
+++ ci3/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://kelompok_tekweb_dse-b/ci3/';
+$config['base_url'] = 'http://localhost/kelompok-_tekweb_dse-b/ci3/';
 
 /*
 |--------------------------------------------------------------------------
--- ci3/application/controllers/Portfolio1.php
+++ ci3/application/controllers/Portfolio1.php
@@ -1,10 +1,10 @@
 <?php
-class Home extends CI_Controller
+class Portfolio1 extends CI_Controller
 {
     function index()
     {
         $this->load->view('templates/navbar');
-        $this->load->view('pages/halamanUtama');
+        $this->load->view('pages/portfolio1');
         $this->load->view('templates/footer');
     }
 }
