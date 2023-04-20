f3b3e8e0b72239bb9bca1f404a4e0814069c9553
kukuhyulionsetiaprakoso <kukuhyulionsetiaprakoso@gmail.com>
2023-04-20 11:22:27 +0700

    update

--- ci3/application/views/pages/portfolio1.php
+++ ci3/application/views/pages/portfolio1.php
@@ -1,5 +1,9 @@
-<br /><br /><br />
-<div class="container">
-    <h1 class="display-4"><img src=" http://localhost/kelompok-_tekweb_dse-b/ci3/asset/bgmerah2.jpg" /></h1>
+<section class="biodata" id="biodata">
+    <div class="container">
+        <div class="row">
+            <div class="col">
 
-</div>
+            </div>
+        </div>
+    </div>
+</section>
