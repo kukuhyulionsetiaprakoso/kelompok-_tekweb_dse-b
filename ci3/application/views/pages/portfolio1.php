5bce6bafbbff7e057ab7001a7824e2e8c7861242
kukuhyulionsetiaprakoso <kukuhyulionsetiaprakoso@gmail.com>
2023-04-20 11:28:51 +0700

    update

--- ci3/application/views/pages/portfolio1.php
+++ ci3/application/views/pages/portfolio1.php
@@ -1,9 +1,21 @@
+<!-- biodata-->
 <section class="biodata" id="biodata">
+    <br /><br />
     <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <div class="gambarbio">
+                    <h1 class="fw-bold">DATA DIRI</h1>
+                    <img src="http://localhost/kelompok-_tekweb_dse-b/ci3/asset/bgmerah2.jpg" class="img-thumbnail rounded-circle rounded mx-auto d-block" alt="...">
+                </div>
+            </div>
+        </div>
         <div class="row">
-            <div class="col">
-
+            <div class="col-12">
+                <h1 class="text-center fw-bold">Kukuh Yulion Setia Prakoso</h1>
             </div>
         </div>
     </div>
+    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 </section>
+<!--Akhir Visi Misi-->
--- ci3/application/views/templates/footer.php
+++ ci3/application/views/templates/footer.php
@@ -26,7 +26,6 @@
 </section>
 <!--akhir bottom-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="http://localhost/kelompok-_tekweb_dse-b/ci3/js/halamanUtama.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </body>
 
